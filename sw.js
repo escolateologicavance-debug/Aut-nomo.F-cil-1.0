d9b6e993facbc2f7b66fd8820b18a4a056e6835c
Gilberto Moreira <escolateologicavance@gmail.com>
2026-06-02 11:57:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE =
+"autonomofacil-v1";
+
+const urls = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+];
+
+self.addEventListener("install", e => {
+
+e.waitUntil(
+
+caches.open(CACHE)
+.then(cache => cache.addAll(urls))
+
+);
+
+});
+
+self.addEventListener("fetch", e => {
+
+e.respondWith(
+
+caches.match(e.request)
+.then(resp => resp || fetch(e.request))
+
+);
+
+});
